6b71c13c2dbdb200e27bfac5fecdbedb9f16b69a
Ethan Wang <942932864@qq.com>
2021-12-01 15:05:04 +0800

    1. net_visualization_pytorch start!
    ###
    The reason might be the difference between pytorch seed, however I have no idea how to deal with this.
    ###

--- assignment3/cs231n/net_visualization_pytorch.py
+++ assignment3/cs231n/net_visualization_pytorch.py
@@ -5,6 +5,7 @@ import numpy as np
 from .image_utils import SQUEEZENET_MEAN, SQUEEZENET_STD
 from scipy.ndimage.filters import gaussian_filter1d
 
+
 def compute_saliency_maps(X, y, model):
     """
     Compute a class saliency map using the model for images X and labels y.
@@ -41,6 +42,7 @@ def compute_saliency_maps(X, y, model):
     #                             END OF YOUR CODE                               #
     ##############################################################################
     return saliency
+
 
 def make_fooling_image(X, target_y, model):
     """
@@ -84,6 +86,7 @@ def make_fooling_image(X, target_y, model):
     ##############################################################################
     return X_fooling
 
+
 def class_visualization_update_step(img, model, target_y, l2_reg, learning_rate):
     ########################################################################
     # TODO: Use the model to compute the gradient of the score for the     #
@@ -112,6 +115,7 @@ def preprocess(img, size=224):
     ])
     return transform(img)
 
+
 def deprocess(img, should_rescale=True):
     transform = T.Compose([
         T.Lambda(lambda x: x[0]),
@@ -122,10 +126,12 @@ def deprocess(img, should_rescale=True):
     ])
     return transform(img)
 
+
 def rescale(x):
     low, high = x.min(), x.max()
     x_rescaled = (x - low) / (high - low)
     return x_rescaled
+
 
 def blur_image(X, sigma=1):
     X_np = X.cpu().clone().numpy()
@@ -133,6 +139,7 @@ def blur_image(X, sigma=1):
     X_np = gaussian_filter1d(X_np, sigma, axis=3)
     X.copy_(torch.Tensor(X_np).type_as(X))
     return X
+
 
 def jitter(X, ox, oy):
     """
